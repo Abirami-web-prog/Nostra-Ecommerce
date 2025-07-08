f10bd8c56c6a62e60f4654d601417821deb4cba0
Abirami-web-prog <abisscse@gmail.com>
2025-07-08 17:55:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var sidenav=document.querySelector(".side-navbar")
+sidenav.style.left = "-80%"
+function showNavbar(){
+    sidenav.style.left="0"
+}
+function closeNavbar(){
+    sidenav.style.left = "-80%"
+}
